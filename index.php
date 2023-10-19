550ac735fbb8a1c51e9898b4ee30246c05e4dc0f
Leonardo Miguel <leonardomiguelvs84@gmail.com>
2023-10-19 17:10:19 -0300

    implementação do menu

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!----======== CSS ======== -->
+    <link rel="stylesheet" href="./public/css/slidemenu.css">
+    
+    <!----===== Boxicons CSS ===== -->
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+
+    <title>Vcards</title>
+    
+</head>
+<body>
+    <?php
+        include '.\public\header.php'
+    ?>
+
+    <section class="home">
+        <div class="text">Dashboard Sidebar</div>
+    </section>
+
+
+
+</body>
+</html>
