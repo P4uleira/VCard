17748a0f8a5ec512825e8abcf05a612e320adcef
P4uleira <pricardoporfirio2001@gmail.com>
2023-10-25 21:33:28 -0300

    build: correção de estrutura de projeto

--- header.php
+++ header.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <!----======== CSS ======== -->
-    <link rel="stylesheet" href="/VCards/public/css/slidemenu.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="./public/css/slidemenu.css">
+    
     <!----===== Boxicons CSS ===== -->
     <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
     
@@ -19,26 +19,21 @@
                 <span class="image">
                     <!--<img src="logo.png" alt="">-->
                 </span>
-
                 <div class="text logo-text">
                     <div style="display: flex; justify-content: center; margin: 0.5rem 0;">
-                      <img id="logo_menu" src="/VCards/public/imgs/logo_vcards.png" alt="logo vcards" width="25px">
+                      <img id="logo_menu" src="./public/imgs/logo_vcards.png" alt="logo vcards" width="25px">
                     </div>
                     <span class="profession">Vcards</span>
                 </div>
             </div>
-
             <i class='bx bx-chevron-right toggle'></i>
         </header>
-
         <div class="menu-bar">
             <div class="menu">
-
                 <li class="search-box">
                     <i class='bx bx-search icon'></i>
                     <input type="text" placeholder="Search...">
                 </li>
-
                 <ul class="menu-links">
                     <li class="nav-link">
                         <a href="#">
@@ -46,45 +41,38 @@
                             <span class="text nav-text">Escanear QRcode</span>
                         </a>
                     </li>
-
                     <li class="nav-link">
                         <a href="#">
                             <i class='bx bx-bar-chart-alt-2 icon' ></i>
                             <span class="text nav-text">Meus favoritos</span>
                         </a>
                     </li>
-
                     <li class="nav-link">
                         <a href="#">
                             <i class='bx bx-bell icon'></i>
                             <span class="text nav-text">Todos os cards</span>
                         </a>
                     </li>
-
                     <li class="nav-link">
                         <a href="#">
                             <i class='bx bx-pie-chart-alt icon' ></i>
                             <span class="text nav-text">Analytics</span>
                         </a>
                     </li>
-
                     <li class="nav-link">
                         <a href="#">
                             <i class='bx bx-heart icon' ></i>
                             <span class="text nav-text">Likes</span>
                         </a>
                     </li>
-
                     <li class="nav-link">
                         <a href="#">
                             <i class='bx bx-wallet icon' ></i>
                             <span class="text nav-text">Wallets</span>
                         </a>
                     </li>
-
                 </ul>
             </div>
-
             <div class="bottom-content">
                 <li class="">
                     <a href="#">
@@ -92,14 +80,12 @@
                         <span class="text nav-text">Sair</span>
                     </a>
                 </li>
-
                 <li class="mode">
                     <div class="sun-moon">
                         <i class='bx bx-moon icon moon'></i>
                         <i class='bx bx-sun icon sun'></i>
                     </div>
                     <span class="mode-text text">Dark mode</span>
-
                     <div class="toggle-switch">
                         <span class="switch"></span>
                     </div>
@@ -107,10 +93,7 @@
                 
             </div>
         </div>
-
     </nav>
-
-
     <script>
         const body = document.querySelector('body'),
         sidebar = body.querySelector('nav'),
@@ -119,29 +102,23 @@
         modeSwitch = body.querySelector(".toggle-switch"),
         modeText = body.querySelector(".mode-text");
         imgLogo = body.querySelector('#logo_menu');
-
-
         toggle.addEventListener("click" , () =>{
             sidebar.classList.toggle("close");
         })
-
         searchBtn.addEventListener("click" , () =>{
             sidebar.classList.remove("close");
         })
-
         modeSwitch.addEventListener("click" , () =>{
             body.classList.toggle("dark");
             
             if(body.classList.contains("dark")){
                 modeText.innerText = "Light mode";
-                imgLogo.src = "/VCards/public/imgs/logo_vcards_branco.png" ;
-
+                imgLogo.src = './public/imgs/logo_vcards_branco.png';
+                
             }else{
                 modeText.innerText = "Dark mode";
-                imgLogo.src = './VCards/public/imgs/logo_vcards.png';
+                imgLogo.src = './public/imgs/logo_vcards.png';
             }
-
-
         });
     </script>
 </body>
--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <!----======== CSS ======== -->
-    <link rel="stylesheet" href="/Vcards/public/css/reset.css">
-    <link rel="stylesheet" href="/Vcards/public/css/slidemenu.css">
+    <link rel="stylesheet" href="./public/css/slidemenu.css">
+
     <!----===== Boxicons CSS ===== -->
     <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
 
     <title>Vcards</title>
-    
+
 </head>
 <body>
     <?php
-        include 'header.php'
+        include '.\public\header.php'
     ?>
 
     <section class="home">
