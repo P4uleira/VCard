47dad69e1c174ceb8b9c50500d6ad40ee4e86ee6
P4uleira <pricardoporfirio2001@gmail.com>
2023-10-29 12:07:43 -0300

    Iniciado o template dos cards

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -22,8 +22,6 @@
     <section class="home">
         <div class="text">Dashboard Sidebar</div>
     </section>
-
-
 
 </body>
 </html>
