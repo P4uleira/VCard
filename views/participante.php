221f18ebbc2b8ff636b497e8e74b9518926f1e80
P4uleira <pricardoporfirio2001@gmail.com>
2023-11-15 12:18:18 -0300

    Iniciado tela participante

--- views/participante.php
+++ views/participante.php
@@ -7,19 +7,53 @@
 </head>
 <body>
     <header>
-        <?php include 'headerParticipante.php'; ?>
+        <?php
+            include './headerParticipante.php'; 
+        ?>
     <header>
     
     <main>
-    <?php
-    if(isset($_GET['modo'])){
-        $modo = $_GET['modo'];
-        if($modo == 'cCard'){           
-    ?>
-            <div class="container eEvento">
+        <?php
+            if(isset($_GET['modo'])){
+                $modo = $_GET['modo'];
+                if($modo == 'cCard'){           
+        ?>
+            <div class="container cCard">
                 
 
             </div>
+
+        <?php
+                }else if($modo == 'eCard'){
+        ?>
+            <form action="../model/criaCard.php" method="POST">
+                <h3 style="text-align: center; margin-bottom: 1.5rem">Cria Card</h3>
+                <div class="container eCard">
+                    <input type="text" placeholder="Titulo" class="form-control" name="titulo">
+                    <span class="highlight"></span>
+                    <span class="bar"></span>
+
+                    <input type="text" placeholder="Descrição" class="form-control" name="descricao">
+                    <span class="highlight"></span>
+                    <span class="bar"></span>
+
+                    <input type="text" placeholder="Descrição" class="form-control" name="descricao">
+                    <span class="highlight"></span>
+                    <span class="bar"></span>
+                </div>
+            </form>          
+        <?php
+                }else if($modo == 'excCard'){
+        ?>
+
+            <div class="container excCard">
+
+            </div>
+
+        <?php
+                }
+            }
+        ?>
     </main>
 </body>
 </html>
