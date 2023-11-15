ab5f5dbd4e44055d89e72ed686249a0f9cd5a75d
Fabricio <fabriciobpinheiro@gmail.com>
2023-11-15 16:27:16 -0300

    ajustes participante

--- views/participante.php
+++ views/participante.php
@@ -28,36 +28,39 @@
             if ($modo == 'cCard') {
         ?>
             <div class="container eEvento">
-                <form action="" class="login_form" method="post" enctype="multipart/form-data">
-                    <h5 style="text-align: center; margin-bottom: 1.5rem">Criar Card</h5>
-                    <div class="form-group">
-                        <label class="custum-file-upload inputadc" for="imagem">Capa do Card</label>                                                
-                            <input class="custom-file-input" placeholder="Selecione uma Imagem" type="file" name="imagem" id="imagem"> 
+                    <form class="form-group file-upload-form">
+                    <label for="imagem" class="file-upload-label">
+                    <div class="file-upload-design">
+                        <svg viewBox="0 0 640 512" height="1em">
+                        <path
+                        d="M144 480C64.5 480 0 415.5 0 336c0-62.8 40.2-116.2 96.2-135.9c-.1-2.7-.2-5.4-.2-8.1c0-88.4 71.6-160 160-160c59.3 0 111 32.2 138.7 80.2C409.9 102 428.3 96 448 96c53 0 96 43 96 96c0 12.2-2.3 23.8-6.4 34.6C596 238.4 640 290.1 640 352c0 70.7-57.3 128-128 128H144zm79-217c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l39-39V392c0 13.3 10.7 24 24 24s24-10.7 24-24V257.9l39 39c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-80-80c-9.4-9.4-24.6-9.4-33.9 0l-80 80z"
+                        ></path>
+                        </svg>
+                    <p>Insira a capa do card aqui</p>
                     </div>
+                    <input id="imagem" type="file" name="imagem"/>
+                    </label>
+                    </form>
                     <div class="form-group">                        
-                        <input class=" form-control" placeholder="Titulo" name="titulo">
+                        <input class=" form-control" placeholder="Título" name="titulo">
                     </div>
                     <div class="form-group">
-                        <textarea name="conteudo"  class=" form-control" rows="5" placeholder="Conteudo da Apresentação"></textarea>
+                        <textarea name="conteudo"  class=" form-control" rows="5" placeholder="Conteúdo da Apresentação"></textarea>
                     </div>
 
-                    <!-- trocar por um select -->
-                    <div class="form-group">
-                        <input name="categoria" class=" form-control" placeholder="Categoria">
-                    </div>
-                    <div class="form-group">
-                        <input name=" class=" form-control" placeholder="URLs">
-                    </div>
+                    <select class="form-select" name="categoria" id="categoria">
+                    <option value="0">Selecione uma Categoria</option>
+                    </select><br>
                     
                     <h5>Informações para contato</h5>
                     <div class="form-group">
-                        <input name=" class=" form-control" placeholder="Telefone">
+                    <img src="../public/imgs/phone.png"><input name="telefone" class=" form-control" placeholder="Telefone">
                     </div>
                     <div class="form-group">
-                        <input name=" class=" form-control" placeholder="Email">
-                    </div>
+                    <img src="../public/imgs/email.png"><input  name="email" class=" form-control" placeholder="Email">
+                    </div><br>
                     
-                    <button class="animated-button btn-block">
+                    <button class="buttonPart btn-block">
                         <span>Cadastrar</span>
                         <span></span>
                     </button>
@@ -68,14 +71,41 @@
                 <?php
             } else if ($modo == 'eCard') {
                 ?>
+            <div class="container eEvento">
+                <form class="form-group"> 
+                    <div class="form-group">
+                    <select class="form-select" name="editarCard" id="editarCard">
+                    <option value="0">Selecione um cartão</option>
+                    </select><br>
+                    </div><br>
+                    
+                    <button class="buttonPart btn-block">
+                        <span>Cadastrar</span>
+                        <span></span>
+                    </button>
+                    </div>
+                </form>
+            </div>
 
                 <?php
-            } else if ($modo == 'excCard') {
+            } else if ($modo == 'exCard') {
                 ?>
 
-                        <div class="container excCard">
-
-                        </div>
+            <div class="container eEvento">
+                <form class="form-group"> 
+                    <div class="form-group">
+                    <select class="form-select" name="excluirCard" id="excluirCard">
+                    <option value="0">Selecione um cartão</option>
+                    </select><br>
+                    </div><br>
+                    
+                    <button class="buttonPart btn-block">
+                        <span>Cadastrar</span>
+                        <span></span>
+                    </button>
+                    </div>
+                </form>
+            </div>
 
                 <?php
             }
