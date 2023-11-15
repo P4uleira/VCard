55d0623c4bb1985c189bdec05bf9ccd3ba3ee45c
Leonardo Miguel <leonardomiguelvs84@gmail.com>
2023-11-15 13:19:05 -0300

    Att no headerVisitante

--- views/headerVisitante.php
+++ views/headerVisitante.php
@@ -6,7 +6,7 @@
         <div class="header">
             <div class="header_logo">
                 <a class="tituloPrincipal" href="visitante.php">
-                    <img width="120px" src="../public/imgs/vcardsHeader.png" alt="logo vCards">
+                    <img width="60px" src="../public/imgs/logo_vcards_branco.png" alt="logo vCards">
                 </a>
             </div>
         </div>
@@ -18,7 +18,7 @@
         </label>
         <div id="sidebarMenu">
             <ul class="sidebarMenuInner">
-            <div style="display: flex; align-items: center; flex-direction: column; margin-top: 1.5rem">
+            <div style="display: flex; align-items: center; flex-direction: column; margin-top: 2.5rem">
                 
                 <a class="tituloPrincipal" href="index.php">
                     <div style="width: 100px; height: 100px; border-radius: 100%; background-image: url('../public/imgs/mm.jpg'); background-size: cover"></div>
@@ -27,15 +27,14 @@
             <?php
                 if (isset($_SESSION['logado'])) {
                     $usuario = $_SESSION['user_nickname'];
-                    echo "<p style=\"color: white; margin-top: 0.5rem\">$usuario</p>";
+                    echo "<p style=\"color: white; margin-top: 0.5rem; font-size:1.2rem\">$usuario</p>";
                 }else {
-                    echo "<p style=\"color: white; margin-top: 0.5rem\">Login</p>";
+                    echo "<p style=\"color: white; margin-top: 0.5rem; font-size:1.2rem\">Login</p>";
                 }                
             ?>
             </div>
                 
-                <li><a style="color: #F0F0F0;" href="meusAlugados.php">Escanear QRcode</a></li>
-                <br>
+                <li><a style="color: #F0F0F0;" href="meusAlugados.php">Escanear QRcode</a></li>                
                 <li><a style="color: #F0F0F0;" href="index.php">Meus favoritos</a></li>
                 <li><a style="color: #F0F0F0;" href="index.php">Todos os cards</a></li>
                 <li><a style="color: #F0F0F0;" href="login.php?sair"><strong>Sair</strong></a></li>
